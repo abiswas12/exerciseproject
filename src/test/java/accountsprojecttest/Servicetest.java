add56c5de5d2330fc139c75a89743464a9e6c3a2
abiswas12 <anirban.biswas@hotmail.com>
2018-03-28 09:38:29 +0100

    TDD is at red stage

--- src/test/java/accountsprojecttest/Servicetest.java
+++ src/test/java/accountsprojecttest/Servicetest.java
@@ -1,14 +1,23 @@
+package accountsprojecttest;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import accountsproject.Service;
 
 public class Servicetest {
-
+	
+	Service service;
+	
+	@Before
+	public void beforetest() {
+		service = new Service();
+	}
+	
 	@Test
 	public void test() throws Exception {
-        Service service = new Service();
+        
         String jsonconvert = "{\"0\":{\"firstname\":\"Matt\",\"secondname\":\"Moore\",\"accountNo\":\"0\"},\"1\":{\"firstname\":\"Nick\",\"secondname\":\"Watton\",\"accountNo\":\"1\"},\"2\":{\"firstname\":\"Anirban\",\"secondname\":\"Biswas\",\"accountNo\":\"2\"},\"3\":{\"firstname\":\"Andrew\",\"secondname\":\"McGill\",\"accountNo\":\"3\"}}";
 		String jsonmap;
         
@@ -27,6 +36,21 @@ public class Servicetest {
 		service.removeAcc(2);
 		service.removeAcc(1);
 		service.removeAcc(0);
+	}
+	
+	@Test
+	public void test2() {
+		String rptdFistN = "Anirban";
+		String rptdSecondN = "Biswas";
+		service.addAcc("Matt", "Moore", "0");
+		service.addAcc("Nick", "Watton", "1");
+		service.addAcc(rptdFistN, rptdSecondN, "2");
+		service.addAcc("Andrew", "McGill", "3");
+		service.addAcc(rptdFistN, rptdSecondN, "4");
+		service.addAcc(rptdFistN, rptdSecondN, "5");
+		service.addAcc(rptdFistN, rptdSecondN, "6");
+		int numbcheck = service.countAccount(rptdFistN);
+		assertEquals(4, numbcheck);
 	}
 
 }
